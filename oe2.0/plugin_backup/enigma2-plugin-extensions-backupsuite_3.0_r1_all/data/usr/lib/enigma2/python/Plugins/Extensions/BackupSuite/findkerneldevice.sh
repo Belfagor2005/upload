f97fdc6c6a2a12647fd718a5a52c1d0705426173
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-08-07 11:07:49 +0200

    -

--- oe2.0/plugin_backup/enigma2-plugin-extensions-backupsuite_3.0_r1_all/data/usr/lib/enigma2/python/Plugins/Extensions/BackupSuite/findkerneldevice.sh
+++
@@ -1,9 +0,0 @@
-args=`cat /proc/cmdline`
-for line in ${args};
-do
-     key=${line%%=*}
-     value=${line#*=}
-     if [ "$key" == "kernel" ]; then
-          ln -sf "$value" /dev/kernel
-     fi
-done
